657859b6574c8a080ae89f8ad0ef27777e7171d0
전승원 <jswa7308@naver.com>
2022-03-10 23:28:06 +0900

    :sparkles: :: Setting library

---
+++ src/main/java/com/example/msgadmin/Controller/BoardController.java
@@ -0,0 +1,13 @@
+package com.example.msgadmin.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+
+    @GetMapping("/board")
+    public String Board() {
+        return "hi2";
+    }
+}
---
+++ src/test/java/com/example/msgadmin/MsgAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msgadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
